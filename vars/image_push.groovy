df58712f3bfdb6eea39e6a7948dbcc36a8459681
Adityasinhase <112686434+Adityasinhase@users.noreply.github.com>
2026-08-10 04:15:51 +0530

    Update image_push.groovy

--- vars/image_push.groovy
+++ vars/image_push.groovy
@@ -1,7 +1,7 @@
 def call(String dockerhubcreds, String imagename, String version){
    withCredentials([usernamePassword(credentialsId:"${dockerhubcreds}",
-                    passwordVariable: "$dockerHubPassword",
-                    usernameVariable: "$dockerHubUsername")])
+                    passwordVariable: "dockerHubPassword",
+                    usernameVariable: "dockerHubUsername")])
                 {
                 sh "docker login -u ${env.dockerHubUsername} -p ${env.dockerHubPassword}"
                 sh "docker image tag ${imagename} ${env.dockerHubUsername}/${imagename}:${version}"
