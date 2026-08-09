c54c648345142b5b79ed0c511cbb120a66bc58d6
Adityasinhase <112686434+Adityasinhase@users.noreply.github.com>
2026-08-10 04:03:20 +0530

    Update image_push.groovy

--- vars/image_push.groovy
+++ vars/image_push.groovy
@@ -1,10 +1,10 @@
-def call(String dockerhubcreds, String dockerHubPassword, String dockerHubUsername){
+def call(String dockerhubcreds, String imagename, String version){
    withCredentials([usernamePassword(credentialsId:"${dockerhubcreds}",
-                    passwordVariable: "${dockerHubPassword}",
-                    usernameVariable: "${dockerHubUsername}")])
+                    passwordVariable: "$dockerHubPassword",
+                    usernameVariable: "$dockerHubUsername")])
                 {
                 sh "docker login -u ${env.dockerHubUsername} -p ${env.dockerHubPassword}"
-                sh "docker image tag flask_app_image ${env.dockerHubUsername}/flask_app_image:latest"
-                sh "docker push ${env.dockerHubUsername}/flask_app_image:latest"
+                sh "docker image tag flask_app_image ${env.dockerHubUsername}/$imagename:$version"
+                sh "docker push ${env.dockerHubUsername}/$imagename:$version"
                 }
 }
